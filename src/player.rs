b18ba7570016d2d4c29879abc2b610f56f045fbb
JunkyDeveloper <junkydeveloper@gmail.com>
2026-09-05 17:08:36 +0200

    go with test server because steel already has somehting like that

--- src/player.rs
+++ src/player.rs
@@ -274,7 +274,7 @@ impl FlintPlayer for SteelTestPlayer {
     }
 
     fn set_game_mode(&mut self, mode: GameMode) -> Result<(), anyhow::Error> {
-        self.player.flint_change_game_mode_state(match mode {
+        self.player.set_game_mode(match mode {
             GameMode::Survival => GameType::Survival,
             GameMode::Creative => GameType::Creative,
             GameMode::Adventure => GameType::Adventure,
